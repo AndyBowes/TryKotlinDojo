f65e809c1d94327ef8a2bf1e0a994c547da60ba5
Andy Bowes <48afe687@opayq.com>
2017-10-18 19:07:14 +0100

    Creation of Pokemon Domain & DAO classes

--- src/test/kotlin/dojo/leeds/problem6/PokemonDAOTest.kt
+++ src/test/kotlin/dojo/leeds/problem6/PokemonDAOTest.kt
@@ -10,44 +10,47 @@ import org.hamcrest.core.IsEqual.equalTo
 import org.junit.Test
 
 
-data class Pokemon(val id: Int, val order: Int, val name:String, val weight:Int, val height:Int, val sprites: Map<String,String?>)
-
 fun readJson(fileName: String): String{
     return String.javaClass.classLoader.getResourceAsStream(fileName)
             .bufferedReader().readLines().joinToString("")
 }
 
-class PokemonDAO {
-    val moshi = Moshi.Builder()
+class PokemonDAOTest {
+
+    val pokemonDao:PokemonDao = PokemonRestDao()
+
+    private val moshi = Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
             .build()
-    val pokemonAdapter = moshi.adapter(Pokemon::class.java)
+    private val pokemonAdapter = moshi.adapter(Pokemon::class.java)
+
+    private fun pokemonUrl(id: Int) = "https://pokeapi.co/api/v2/pokemon/$id/"
 
     fun getPokemonById(id: Int): Pokemon? {
         val headers = mapOf("User-Agent" to "Mozilla/5.0")
-        val (request, response, result) = "https://pokeapi.co/api/v2/pokemon/$id/"
+        val (_, response, result) = pokemonUrl(id)
                 .httpGet().header(headers).responseString()
-        println( request.cUrlString())
-        val json = result.get()
-        val pokemon = pokemonAdapter.fromJson(json)
-        return pokemon
+        return pokemonAdapter.fromJson(result.get())
     }
 
     @Test
     fun `given a valid pokemon id then a populated Pokemon should be returned`(){
         val pokemon = getPokemonById(2)
         assertThat(pokemon, not(nullValue()))
-        if (pokemon != null){
-            pokemon.apply {
-                assertThat(id, equalTo(2))
-                assertThat(order, equalTo(2))
-                assertThat(name, equalTo("ivysaur"))
-                assertThat(height, equalTo(10))
-                assertThat(weight, equalTo(130))
-                assertThat(sprites, not(nullValue()))
-                assertThat(sprites.size, equalTo(8))
-            }
+        pokemon?.apply {
+            assertThat(id, equalTo(2))
+            assertThat(order, equalTo(2))
+            assertThat(name, equalTo("ivysaur"))
+            assertThat(height, equalTo(10))
+            assertThat(weight, equalTo(130))
+            assertThat(sprites, not(nullValue()))
+            assertThat(sprites.size, equalTo(8))
         }
+    }
+
+    @Test
+    fun `fetch many pokemon`(){
+        (21..30).map { getPokemonById(it) }
     }
 }
 
