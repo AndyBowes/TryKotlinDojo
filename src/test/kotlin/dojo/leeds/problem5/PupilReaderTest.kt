a9c5c908a0af0b235c8c034ab442cd81d4dd40cb
Andy Bowes <48afe687@opayq.com>
2017-10-21 15:46:02 +0100

    Add Pupil Tests

--- src/test/kotlin/dojo/leeds/problem5/PupilReaderTest.kt
+++ src/test/kotlin/dojo/leeds/problem5/PupilReaderTest.kt
@@ -6,17 +6,48 @@ import org.hamcrest.core.IsEqual.equalTo
 import org.junit.Assert.*
 import org.junit.Test
 
-/**
- * Created by andybowes on 15/10/2017.
- */
 class PupilReaderTest {
 
-    val pupilReader = PupilReader()
+    private val pupilReader = PupilReader()
+    private val pupils = pupilReader.fetchPupils()
 
     @Test
     fun `given a PupilReader then readFile should return a list of lines `(){
         val lines = pupilReader.readPupilFile()
         assertThat(lines, not(nullValue()))
-        assertThat(lines.count(), equalTo(122))
+        assertThat(lines.count(), equalTo(121))
     }
+
+    @Test
+    fun `given the PupilReader then fetchPupils should return a list of pupils`(){
+        val pupils = pupilReader.fetchPupils()
+        assertThat(pupils, not(nullValue()))
+        assertThat(pupils.count(), equalTo(121))
+    }
+
+    @Test
+    fun `given then count the pupils in each class`(){
+        val pupils = pupilReader.fetchPupils()
+        pupils.groupBy { it.className }
+                .entries.sortedBy { it.key }
+                .forEach{ println("${it.key} : ${it.value.size}")}
+    }
+
+    @Test
+    fun `find the class with the highest average score`(){
+        val pupils = pupilReader.fetchPupils()
+        pupils.groupBy { it.className }
+                .entries.sortedBy { it.key }
+                .map { Pair(it.key, it.value.sumBy { pupil -> pupil.english + pupil.maths + pupil.science }/it.value.size.toDouble()) }
+                .forEach { println("Class ${it.first}  : ${it.second} ")}
+    }
+
+    @Test
+    fun `find pupils who have scored all Grade A's`(){
+        val pupils = pupilReader.fetchPupils()
+        val passGrades = listOf(Grade.A, Grade.B, Grade.C)
+        pupils.filter { it.grades.values.all { it in passGrades }}.forEach { println(it) }
+    }
+
+
 }
